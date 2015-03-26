42e413b67ac61593e6039d474d99fe9c6a32c993
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2015-03-26 02:48:38 +0000

    Add missing variable initializations in unraise.sh
    Fix paths in tests
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- unraise.sh
+++ unraise.sh
@@ -14,6 +14,19 @@ export INST_DIR=${DESTDIR-dist}
 
 INST_DIR=`readlink -f $INST_DIR`
 
+if test $EUID -eq 0
+then
+    export SUDO=""
+elif test ! -f `which sudo 2>/dev/null`
+then
+    echo "Raixen requires sudo to install build dependencies for you."
+    echo "Please install sudo, then run this script again."
+    exit 1
+fi
+
+get_distro
+get_arch
+
 for f in `cat "$BASEDIR"/components/series`
 do
     source "$BASEDIR"/components/"$f"
@@ -24,8 +37,19 @@ for_each_component unconfigure
 
 for i in `cat /var/log/raisin.log 2>/dev/null`
 do
-    rm -rf /"$i"
+    if test -f /"$i"
+    then
+        rm -f /"$i"
+    fi
 done
+for i in `cat /var/log/raisin.log 2>/dev/null`
+do
+    if test -d /"$i"
+    then
+        rmdir --ignore-fail-on-non-empty /"$i"
+    fi
+done
+
 rm -rf /var/log/raisin.log
 rm -rf "$INST_DIR"
 
