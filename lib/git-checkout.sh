5ff88a6954ac5d380e4fa0cb024c0a23954374fa
Doug Goldstein <cardoe@cardoe.com>
2015-09-09 10:08:26 -0500

    Fix up coding style
    Signed-off-by: Doug Goldstein <cardoe@cardoe.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- lib/git-checkout.sh
+++ lib/git-checkout.sh
@@ -3,8 +3,8 @@
 function git-checkout() {
     if [[ $# -lt 3 ]]
     then
-	echo "Usage: $0 <tree> <tag> <dir>"
-	exit 1
+        echo "Usage: $0 <tree> <tag> <dir>"
+        exit 1
     fi
 
     TREE=$1
@@ -15,17 +15,17 @@ function git-checkout() {
 
     if [[ ! -d $DIR-remote ]]
     then
-	rm -rf $DIR-remote $DIR-remote.tmp
-	mkdir -p $DIR-remote.tmp; rmdir $DIR-remote.tmp
-	$GIT clone $TREE $DIR-remote.tmp
-	if [[ "$TAG" ]]
-	then
-	    cd $DIR-remote.tmp
-	    $GIT branch -D dummy >/dev/null 2>&1 ||:
-	    $GIT checkout -b dummy $TAG
-	    cd ..
-	fi
-	mv $DIR-remote.tmp $DIR-remote
+        rm -rf $DIR-remote $DIR-remote.tmp
+        mkdir -p $DIR-remote.tmp; rmdir $DIR-remote.tmp
+        $GIT clone $TREE $DIR-remote.tmp
+        if [[ "$TAG" ]]
+        then
+            cd $DIR-remote.tmp
+            $GIT branch -D dummy >/dev/null 2>&1 ||:
+            $GIT checkout -b dummy $TAG
+            cd ..
+        fi
+        mv $DIR-remote.tmp $DIR-remote
     fi
     rm -f $DIR
     ln -sf $DIR-remote $DIR
